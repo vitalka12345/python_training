3275b936492176f8eb42348d107b739933d5e4d9
vkomarov <vitaliy932@yandex.ru>
2019-07-12 11:47:23 +0300

    Добавлены остальные файлы в репозиторий

---
+++ example1.py
@@ -0,0 +1,7 @@
+def hello(loh):
+     print ("Hello, "+ loh +"! xa-xa!")
+
+
+hello("Lox")
+hello("Pidr")
+hello("Net Druzey")
---
+++ hello.py
@@ -0,0 +1,9 @@
+__author__='pes'
+
+def hello(s):
+    print ("Hello, "+ s +"!")
+
+
+hello("lox")
+hello("pidor")
+hello("net druzey")
